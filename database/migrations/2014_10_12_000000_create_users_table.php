a7c33b069ed709837f71a5fdbb30df6c3b1bef8c
gehadkassap <gohadkasaa@gmail.com>
2023-03-02 10:20:54 +0200

    rola lip gloss project

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname',50);
+            $table->string('lname',30);
+            $table->string('image')->nullable();
+            $table->string('email')->unique();
+            $table->string('phone')->nulalble();
+            $table->enum('role' , ['user' , 'admin' , 'writer'])->default('user');
+            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            // $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
